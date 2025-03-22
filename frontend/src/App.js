c7683abb9f13e23fefdf769bcbb46ba7eb1c5996
gayaa-3 <gayathri271203@gmail.com>
2025-03-22 22:45:39 +0530

    created landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Landing from './landing'; // Update with the correct path
+// Import other components you might have
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} /> {/* Set Landing as your home page */}
+        
+      </Routes>
+    </BrowserRouter>
   );
 }
 
